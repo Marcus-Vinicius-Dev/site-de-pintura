d76d0dcd3fc09a9f01245f50ffb2899fcfa4f197
Marcus-Vinicius-C-S <ciapdivtectic@gmail.com>
2024-06-01 00:02:22 -0300

    galeria com passagem de fotos após ampliação de imagem

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,29 @@ document.addEventListener('DOMContentLoaded', function() {
     const modal = document.querySelector('.modal');
     const imagemModal = document.querySelector('.imagem-modal');
     const fecharModal = document.querySelector('.fechar');
+    const botaoAnterior = document.querySelector('.anterior');
+    const botaoProximo = document.querySelector('.proximo');
+    let indiceAtual = 0;
 
-    miniaturas.forEach(function(miniatura) {
+    miniaturas.forEach(function(miniatura, indice) {
         miniatura.addEventListener('click', function() {
             modal.style.display = 'block';
             imagemModal.src = this.src;
+            indiceAtual = indice;
         });
     });
 
     fecharModal.addEventListener('click', function() {
         modal.style.display = 'none';
+    });
+
+    botaoAnterior.addEventListener('click', function() {
+        indiceAtual = (indiceAtual - 1 + miniaturas.length) % miniaturas.length;
+        imagemModal.src = miniaturas[indiceAtual].src;
+    });
+
+    botaoProximo.addEventListener('click', function() {
+        indiceAtual = (indiceAtual + 1) % miniaturas.length;
+        imagemModal.src = miniaturas[indiceAtual].src;
     });
 });
